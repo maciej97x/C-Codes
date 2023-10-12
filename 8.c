2cae5ce3cf3f31603d81caf9458dd75f3c763455
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 07:54:59 +0000

    Pending changes exported from your codespace

---
+++ 8.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+
+int n = get_int("Write number of calls ");
+
+if (n < 1)
+
+    {
+    printf("Call more often!\n ");
+    }
+
+else if (n >= 1)
+
+    {
+    printf("We have such a good contact!\n ");
+    }
+
+}
+
+
+
---
+++ 9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int lenght;
+    do
+    {
+        lenght = get_int("Lenght ");
+    }
+    while (lenght < 1);
+
+    int twice [lenght];
+
+    twice[0] = 1;
+    printf("%i\n", twice [0]);
+
+    for (int i = 1; i < lenght; i++)
+    {
+        twice[i] = 2 * twice [i - 1];
+        printf("%i\n", twice[i]);
+    }
+
+}
