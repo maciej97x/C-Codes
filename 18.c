ead4cf6616c0ea6433f912cbd583fa9a395b9b5c
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 16:14:07 +0000

    Thu, Oct 12, 2023, 4:14 PM +00:00

---
+++ 18.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string words [2];
+
+    words [0] = "Hi";
+    words [1] = "Bye";
+
+    printf("%s\n", words [0]);
+    printf("%s\n", words [1]);
+
+}
