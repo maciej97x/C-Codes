2be8d10c4a33b350b464f1a92fab3b1034a387b8
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 06:41:07 +0000

    Thu, Oct 12, 2023, 6:41 AM +00:00

---
+++ 4.c
@@ -0,0 +1,41 @@
+int integer - number, height, width
+char characters - letter
+float - sqrt2, pi
+&& - and
+|| - or
+! - not
+== equal
+!= not equal
+while (true) - loop forever
+ctrl+c finish a forever loop, infinite loop
+for (int i = 0; i < 10 ; i++) (i is integer, so 0 i less than ten that is a true, i++ so  0+1 is 1 and again check a + and add more)
+do while same as while but at least once
+for when you want to use a discrete number of times
+ls list of files
+ctr l celar allc
+return ((bill + ((((bill + ((bill * tax)/100))) * tip)) / 100) / 2 );
+% numbers - int i, long li, float f, double f
+% text - char c, string s
+boolean - yes or no
+
+1) preprocessing - thats a part with #, its a library pre process, contain functions,
+2) compiling - compile code like i write
+3) assembling - wierd language that is compiler is compile your code into and then into machine code that computer understand
+4) linking - so every code like cs 50 vs code standard code are creating a together
+
+so 1 is for library its like a book with functions, 2 compile code that i wrote, 3 change into assemble code, 4 connect all together
+1 library > 2 into weird code > 3 machine code/binary code > 4 connect them together
+
+what is intresting that counting start from 0
+
+debug50 ./ and name - to debug
+
+So about a bytes:
+bool 1 byte
+int 4 bytes
+long 8 bytes
+float 4 bytes
+double 8 bytes
+char 1 byte
+string ? bytes
+int indicate main and main mean 0, is hidden.
