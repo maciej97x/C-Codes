b29cf1d1f57ef47526f6220acd564bc372c6634a
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 10:11:09 +0000

    Thu, Oct 12, 2023, 10:11 AM +00:00

--- 11.c
+++ 11.c
@@ -5,21 +5,4 @@
 bool check_phrase(string);
 
 int main(void)
-{
-string phrase = get_string("What your secret phrase? ");
 
-bool correct = check_phrase(phrase);
-if (correct == true)
-
-bool check_phrase(string phrase)
-
-{
-    string password = "Please ";
-    if (phrase == password)
-{
-    return true;
-}
-
-}
-
-}
