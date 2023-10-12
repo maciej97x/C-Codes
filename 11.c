878c8dd5201c9675357ee2de23a91108cbb76119
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 09:18:12 +0000

    Thu, Oct 12, 2023, 9:18 AM +00:00

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+bool check_phrase(string);
+
+int main(void)
+{
+string phrase = get_string("What your secret phrase? ");
+
+bool correct = check_phrase(phrase);
+if (correct == true)
+
+bool check_phrase(string phrase)
+
+{
+    string password = "Please ";
+    if (phrase == password)
+{
+    return true;
