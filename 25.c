29f027b04e0647d84932f684d41543540aa61cdc
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 16:30:33 +0000

    Thu, Oct 12, 2023, 4:30 PM +00:00

--- 25.c
+++ 25.c
@@ -1,35 +1,13 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 #include <string.h>
-
-typedef struct
-{
-    string name;
-    string number;
-}
-person;
 
 int main(void)
 {
-    person people [2];
-
-    people[0].name = "Maciej";
-    people[0].number = "1321231";
-
-    string name = get_string("Name ");
-    for (int i = 0; i < 1; i++)
+    string name = "Emma ";
+    int lenght = strlen(name);
+    for(int i = 0; i < lenght; i++)
     {
-        if (strcmp(people[i].name, name) == 0)
-        {
-            printf("Found %s\n", people[i].number);
-            return 0;
-        }
-        else
-        {
-            printf("Not found\n");
-            return 1;
-        }
-
+        printf("%i ", name[i]);
     }
-
 }
