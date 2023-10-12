56a312983376ecd068273ed4014ad58e1ead3955
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-12 16:21:25 +0000

    Thu, Oct 12, 2023, 4:21 PM +00:00

---
+++ 25.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = "Emma ";
+    int lenght = strlen(name);
+    for(int i = 0; i < lenght; i++)
+    {
+        printf("%i ", name[i]);
+    }
+}
