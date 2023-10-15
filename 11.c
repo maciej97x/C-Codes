31c85b21f288bb3767a6fd53c40381db8c1bfd2a
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-15 13:34:25 +0000

    Sun, Oct 15, 2023, 1:34 PM +00:00

--- 11.c
+++ 11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <stdbool.h>
+#include <string.h>
 
 
 int main(void)
@@ -8,6 +9,9 @@ int main(void)
 string phrase = get_string("What your secret phrase? ");
 string password = ("Mala ");
 
-if (strcmp())
+if (strcmp(phrase, password) == 0)
+{
+    printf("Correct ");
+}
 
 }
