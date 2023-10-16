3eac9a0cd4f8113e7b9313bd19fdc681affd4920
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-16 07:46:20 +0000

    Mon, Oct 16, 2023, 7:46 AM +00:00

--- 11.c
+++ 11.c
@@ -2,46 +2,9 @@
 #include <cs50.h>
 #include <stdbool.h>
 
-bool check_phrase(string);
-
 int main(void)
 {
-string phrase = get_string("What your secret phrase? ");
-
-bool correct = check_phrase(phrase);
-if (correct == true)
-
-bool check_phrase(string phrase)
-
-{
-    string password = "Please ";
-    if (phrase == password)
-{
-    return true;
+    string phrase = get_string("What your secret phrase ");
+    
 }
 
-}
-
-}
-
-bool check_phrase(string);
-
-int main(void)
-{
-string phrase = get_string("What your secret phrase? ");
-
-bool correct = check_phrase(phrase);
-if (correct == true)
-
-bool check_phrase(string phrase)
-
-{
-    string password = "Please ";
-    if (phrase == password)
-{
-    return true;
-}
-
-}
-
-}
