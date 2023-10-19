053306c9088ca4fe9e4ee7dd118f4d0907637659
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-19 10:23:19 +0000

    Thu, Oct 19, 2023, 10:23 AM +00:00

--- 11.c
+++ 11.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
+bool valid(string password);
 int main(void)
 {
     string phrase = get_string("What your secret phrase ");
     string password = ("Please ");
-    
-    bool correct = password;
+
+
+
+
+
+    bool valid(string password)
 
 
 
