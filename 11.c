ea8dcccc9a868ecaae17f4d6732527fd3f6a11a6
maciej97x <142264723+maciej97x@users.noreply.github.com>
2023-10-20 06:57:29 +0000

    Fri, Oct 20, 2023, 6:57 AM +00:00

---
+++ 11.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+bool prime(string number);
+
+int main(void)
+{
+    string password;
+    do
+    {
+        password = get_string(" ");
+    }
+    while (password < 1);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+bool prime(string number)
+{
+
+    number == ("mama");
+    if (number == password)
+    {
+        return true;
+    }
+
+}
